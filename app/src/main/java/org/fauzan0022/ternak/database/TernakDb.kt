33511651516d108b37641b5f27ced8f9b3994e75
FauzanSetengahSalmon <ahmadfauzan201006@gmail.com>
2026-05-06 14:09:41 +0700

    Memperbaiki database ke version 2

--- app/src/main/java/org/fauzan0022/ternak/database/TernakDb.kt
+++ app/src/main/java/org/fauzan0022/ternak/database/TernakDb.kt
@@ -1,16 +1,16 @@
 package org.fauzan0022.ternak.database
 
-import org.fauzan0022.ternak.model.Ternak
-import org.fauzan0022.ternak.model.TernakRecycle
-import org.fauzan0022.ternak.model.Kesehatan
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import org.fauzan0022.ternak.model.Kesehatan
+import org.fauzan0022.ternak.model.Ternak
+import org.fauzan0022.ternak.model.TernakRecycle
 
 @Database(
     entities = [Ternak::class, TernakRecycle::class, Kesehatan::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class TernakDb : RoomDatabase() {
@@ -20,15 +20,21 @@ abstract class TernakDb : RoomDatabase() {
     abstract fun kesehatanDao(): KesehatanDao
 
     companion object {
-        @Volatile private var INSTANCE: TernakDb? = null
+        @Volatile
+        private var INSTANCE: TernakDb? = null
 
         fun getInstance(context: Context): TernakDb {
             return INSTANCE ?: synchronized(this) {
-                Room.databaseBuilder(
+                val instance = Room.databaseBuilder(
                     context.applicationContext,
                     TernakDb::class.java,
                     "ternak_db"
-                ).build().also { INSTANCE = it }
+                )
+                    .fallbackToDestructiveMigration(false)
+                    .build()
+
+                INSTANCE = instance
+                instance
             }
         }
     }
